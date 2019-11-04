2b676dcbe04aa82fcd6b7975b1b5eaa5f6cced97
goknurbati <goknurbati@gmail.com>
2019-11-04 23:23:35 +0300

    Kata-Factorial

---
+++ src/test/java/Factorial/Factorial.java
@@ -0,0 +1,53 @@
+package Factorial;
+
+import java.util.Scanner;
+
+
+public class Factorial {
+
+
+    public static void main(String[] args) {
+
+        for (int enteredNumber = 1; enteredNumber >= 1; enteredNumber++) {
+            
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter your number : ");
+            enteredNumber = input.nextInt();
+
+            if (enteredNumber == 4) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            } else if (enteredNumber == 5) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            } else if (enteredNumber == 6) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            } else if (enteredNumber == 7) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            } else if (enteredNumber == 8) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            } else if (enteredNumber == 9) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            } else if (enteredNumber == 10) {
+                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
+                System.out.println("Result :" + result);
+            }
+
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
