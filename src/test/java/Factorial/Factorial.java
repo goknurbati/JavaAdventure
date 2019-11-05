742b2b5d0e3dd5f22fff82e82a0ff0a04f41aa45
goknurbati <goknurbati@gmail.com>
2019-11-05 23:43:43 +0300

    edit factorial kata

---
+++ src/test/java/Factorial/Factorial.java
@@ -0,0 +1,27 @@
+package Factorial;
+
+public class Factorial {
+    public static void main(String[] args){
+        //System.out.println(calculateFactorialWithLoop(20));
+        System.out.println(calculateFactorialWithRecursive(20));
+    }
+    public static long calculateFactorialWithLoop(int n){
+        long result=1;
+        for(int i=1;i<=n;i++){
+            result=result*i;
+        }
+        return result;
+    }
+    public static long calculateFactorialWithRecursive(int n) {
+        long result = 1;
+        if (n==1) {
+            return result;
+        }
+        else{
+                result = calculateFactorialWithRecursive(n - 1) * n;
+
+            }
+            return result;
+        }
+    }
+
