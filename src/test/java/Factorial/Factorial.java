003fe199399e7cc166b83a91add723cf0b119dd7
goknurbati <goknurbati@gmail.com>
2019-11-05 20:54:21 +0300

    Kata-FActorial edited

--- src/test/java/Factorial/Factorial.java
+++ src/test/java/Factorial/Factorial.java
@@ -16,40 +16,9 @@ public class Factorial {
 
             int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
             System.out.println("Result :" + result);
-
-              /*if (enteredNumber == 3) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 4) {
-                    int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                    System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 5) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 6) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 7) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 8) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 9) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }
-
-              else if (enteredNumber == 10) {
-                int result = (enteredNumber - 2) * (enteredNumber - 1) * enteredNumber;
-                System.out.println("Result :" + result); }*/
         }
     }
+
 }
 
 
