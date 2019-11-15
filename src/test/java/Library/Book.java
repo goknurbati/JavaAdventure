66b5f23f04e811cbe65115156f1b843661b18de8
goknurbati <goknurbati@gmail.com>
2019-11-15 22:32:42 +0300

    add new branch for library project

---
+++ src/test/java/Library/Book.java
@@ -0,0 +1,53 @@
+package Library;
+
+public class Book {
+    int bookID;
+    String bookName;
+    String author;
+    String type;
+    String publisher;
+
+    public Book(){
+        this.bookID=bookID;
+        this.bookName=bookName;
+        this.author= author;
+        this.type=type;
+        this.publisher=publisher;
+    }
+    public int getBookID(){
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    public static class Main{
+        public static void main(String[] args){
+            BookController controller = new BookController();
+        }
+    }
+}
---
+++ src/test/java/Library/BookController.java
@@ -0,0 +1,42 @@
+package Library;
+
+import java.util.ArrayList;
+
+public class BookController {
+
+    public static String addedBook;
+    public static String removedBook;
+    public static int bookCount;
+    public static int book;
+
+    public BookController(){
+        this.addedBook=addedBook;
+        this.removedBook=removedBook;
+        this.bookCount=bookCount;
+        this.book=book;
+    }
+
+    public static ArrayList<Book> books = new ArrayList<Book>();
+
+    public void addBook(){
+
+    }
+    public void updateBook(){
+
+    }
+    public void deleteBook(){
+
+    }
+    public void getAllBook(){
+
+    }
+
+    public static class Main{
+        public static void main(String[] args){
+            Book books = new Book();
+        }
+    }
+
+
+}
+
